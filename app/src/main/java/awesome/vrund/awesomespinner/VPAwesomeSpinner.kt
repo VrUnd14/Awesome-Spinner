c3b03788d86173b6b95f8c4eef7ca4025e0bdbf5
Vrund Purohit <vrund14@gmail.com>
2020-08-14 10:57:26 +0530

    New UI Features

--- app/src/main/java/awesome/vrund/awesomespinner/VPAwesomeSpinner.kt
+++ app/src/main/java/awesome/vrund/awesomespinner/VPAwesomeSpinner.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
 import android.util.AttributeSet
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.RelativeLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.getStringOrThrow
 import kotlinx.android.synthetic.main.vp_awesome_spinner.view.*
 
 class VPAwesomeSpinner @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
@@ -21,20 +24,35 @@ class VPAwesomeSpinner @JvmOverloads constructor(context: Context, attrs: Attrib
     private fun init(attrs: AttributeSet?) {
         View.inflate(context, R.layout.vp_awesome_spinner, this)
         val ta = context.obtainStyledAttributes(attrs, R.styleable.VPAwesomeSpinner)
-        try {
-            val ttl = ta.getString(R.styleable.VPAwesomeSpinner_title)
-            val backColor = ta.getColor(R.styleable.VPAwesomeSpinner_tintColor, Color.BLACK)
-            val textColor = ta.getColor(R.styleable.VPAwesomeSpinner_titleColor, Color.WHITE)
-            spinnerAwesome.tag = ta.getString(R.styleable.VPAwesomeSpinner_spinnerTag)
+
+        val ttl = ta.getString(R.styleable.VPAwesomeSpinner_vp_title)
+        if (ttl != null) {
             title.text = ttl
-            title.setTextColor(textColor)
-            val gd = title.background as GradientDrawable
-            gd.setColor(backColor)
-            curveImg.setColorFilter(backColor, PorterDuff.Mode.SRC_ATOP)
-            spnDrop.setColorFilter(backColor, PorterDuff.Mode.SRC_ATOP)
-        } finally {
-            ta.recycle()
         }
+        val textColor = ta.getColor(R.styleable.VPAwesomeSpinner_vp_titleColor, Color.WHITE)
+        title.setTextColor(textColor)
+
+        val tintColor = ta.getColor(R.styleable.VPAwesomeSpinner_vp_tintColor, Color.BLACK)
+        val gd = title.background as GradientDrawable
+        gd.setColor(tintColor)
+        curveImg.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP)
+        spnDrop.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP)
+
+        val ld = spinnerAwesome.background as LayerDrawable
+
+        val backColor = ta.getColor(R.styleable.VPAwesomeSpinner_vp_backColor, ContextCompat.getColor(context, R.color.vp_back_color))
+        val backGD = ContextCompat.getDrawable(context, R.drawable.vp_back) as GradientDrawable
+        backGD.setColor(backColor)
+        ld.setDrawableByLayerId(R.id.back, backGD)
+
+        val dividerColor = ta.getColor(R.styleable.VPAwesomeSpinner_vp_backColor, ContextCompat.getColor(context, R.color.vp_divider_color))
+        val dividerGD = ContextCompat.getDrawable(context, R.drawable.vp_divider) as GradientDrawable
+        dividerGD.setColor(dividerColor)
+        ld.setDrawableByLayerId(R.id.divider, dividerGD)
+
+        spinnerAwesome.tag = ta.getStringOrThrow(R.styleable.VPAwesomeSpinner_vp_spinnerTag)
+
+        ta.recycle()
     }
 
     fun setTitle(ttl: String) {
@@ -50,6 +68,22 @@ class VPAwesomeSpinner @JvmOverloads constructor(context: Context, attrs: Attrib
         gd.setColor(color)
         curveImg.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
         spnDrop.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+    }
+
+    fun setBackColor(color: Int) {
+        val ld = spinnerAwesome.background as LayerDrawable
+
+        val gd = ContextCompat.getDrawable(context, R.drawable.vp_back) as GradientDrawable
+        gd.setColor(color)
+        ld.setDrawableByLayerId(R.id.back, gd)
+    }
+
+    fun setDividerColor(color: Int) {
+        val ld = spinnerAwesome.background as LayerDrawable
+
+        val gd = ContextCompat.getDrawable(context, R.drawable.vp_divider) as GradientDrawable
+        gd.setColor(color)
+        ld.setDrawableByLayerId(R.id.divider, gd)
     }
 
     fun setAdapter(adapter: ArrayAdapter<*>?) {
